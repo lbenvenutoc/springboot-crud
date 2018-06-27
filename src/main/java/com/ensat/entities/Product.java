a21498230755c0f8c26eb491b793a88c2cc14a77
lbenvenutoc <lbenvenutoc@gmail.com>
2018-06-26 19:43:19 -0500

    Primera subida demo Spring Boot CRUD

---
+++ src/main/java/com/ensat/entities/Product.java
@@ -0,0 +1,78 @@
+package com.ensat.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Version;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import org.hibernate.validator.constraints.NotBlank;
+
+import java.math.BigDecimal;
+
+/**
+ * Product entity.
+ */
+@Entity
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    @Version
+    private Integer version;
+    
+    
+    @NotBlank(message = "El ID de producto no puede ser nulo o en blanco")
+    private String productId;
+    
+    @Size(min=2, max=30, message="El nombre debe tener entre 2 y 30 caracteres")
+    private String name;
+    
+    @NotNull(message="El precio no puede ser nulo o en blanco")      
+    private BigDecimal price;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getVersion() {
+        return version;
+    }
+
+    public void setVersion(Integer version) {
+        this.version = version;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getProductId() {
+        return productId;
+    }
+
+    public void setProductId(String productId) {
+        this.productId = productId;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+}
